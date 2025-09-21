edb619a766009db622f29bc14631e19a3ba9109b
Swathi Vijaykumar <161469903+SWATHI-VIJAY@users.noreply.github.com>
2025-09-21 13:40:05 +0530

    Time: 134 ms (72.28%) | Memory: 41.6 MB (78.20%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -15,8 +15,7 @@ class Solution {
 
     public static boolean dfs(char[][] board, String word, int i, int j, int index) {
         if (index == word.length()) return true;
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length ||
-            board[i][j] != word.charAt(index)) return false;
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length|| board[i][j]!=word.charAt(index)) return false;
 
         char temp = board[i][j];
         board[i][j] = '#'; // mark visited
