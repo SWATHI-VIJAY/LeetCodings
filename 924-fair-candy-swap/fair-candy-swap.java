f46435e7e4007b56d169f5da647dfef2872d7baf
Swathi Vijaykumar <161469903+SWATHI-VIJAY@users.noreply.github.com>
2025-10-22 15:58:14 +0530

    Time: 216 ms (10.61%) | Memory: 45.9 MB (60.57%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.java
+++ 924-fair-candy-swap/fair-candy-swap.java
@@ -1,36 +1,22 @@
 class Solution {
-    private int binarySearch(int[] arr,int target){ 
-
-        int s = 0, e = arr.length-1;
-        
-        while(s <= e)
-        {
-            int mid = s + (e-s)/2;
-            if(arr[mid] == target) return mid;
-            else if(arr[mid] < target) s=mid+1;
-            else e = mid - 1;
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int alice=0;
+        int bob=0;
+        for(int candies:aliceSizes){
+            alice+=candies;
         }
-        return -1;
-    }
-    
-    public int[] fairCandySwap(int[] a, int[] b) { //a = Alice , b=Bob
-
-        int suma=0,sumb=0;
-        for(int i = 0; i < a.length; i++)
-            suma += a[i];
-        
-        for(int i = 0; i < b.length; i++)
-            sumb +=b [i];
-        
-        int diff = (suma - sumb)/2;
-        
-        Arrays.sort(a);
-        for(int num : b)
-        {
-            if (binarySearch(a, num + diff) !=-1) 
-                return new int[] { num + diff, num };
+        for(int candies:bobSizes){
+            bob+=candies;
         }
+        int m=aliceSizes.length, n=bobSizes.length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if((alice-aliceSizes[i]+bobSizes[j])==(bob-bobSizes[j]+aliceSizes[i])){
+                    return new int[]{aliceSizes[i],bobSizes[j]};
+                }
+            }
+        }
+        return new int[0];
         
-        return null;
     }
 }
